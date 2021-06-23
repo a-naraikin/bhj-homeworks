5e9ace29842e4faa6de313979b67b9d9f432894f
anna12god <anna12god@yandex.ru>
2021-06-23 23:52:00 +0300

    completed task

---
+++ element-search/menu/task.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const menu__links = document.querySelectorAll('.menu__link');
+
+menu__links.forEach(elem => elem.onclick = showMenu);
+
+function showMenu() {
+  const menu_active = document.querySelector('.menu_active');
+
+  if (menu_active !== null) {
+    menu_active.classList.toggle('menu_active');
+  }
+
+  if (menu_active !== this.nextElementSibling) {
+    this.nextElementSibling.classList.toggle('menu_active');
+    return false;
+  }
+}
